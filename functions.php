bcd784b31b9b836a3aa97f5fb714a090fd00a27d
Gilles Lengy <hello@gilleslengy.dev>
2023-02-07 14:03:11 +0100

    First reusable pattern block, without image but a message inviting to put one.

--- functions.php
+++ functions.php
@@ -11,16 +11,20 @@ function wp00000001_my_block_patterns() {
             'description' => _x( 'The part of a project that is always displayed', 'wp-2023-child-theme-00000001' ),
             'categories'  => array( 'columns' ),
             'content'     => '
-<!-- wp:heading {"textAlign":"center","level":3} -->
-<h3 class="has-text-align-center"><a href="http://01010101.one" target="_blank" rel="noreferrer noopener">ONE HUMAN FACTOR</a></h3>
+<!-- wp:heading -->
+<h2><a href="https://01010101.one" target="_blank" rel="noreferrer noopener">One Human Factor</a></h2>
 <!-- /wp:heading -->
 
-<!-- wp:image {"id":19,"width":1500,"height":1000,"sizeSlug":"full","linkDestination":"media","className":"is-style-twentytwentyone-border"} -->
-<figure class="wp-block-image size-full is-resized is-style-twentytwentyone-border"><a href="https://ddf8-728a63f95197.wptiger.fr/wp-content/uploads/2021/04/ohf-roc-gam-2k19-1221-by-laurent-t-06-1500.jpg"><img src="https://ddf8-728a63f95197.wptiger.fr/wp-content/uploads/2021/04/ohf-roc-gam-2k19-1221-by-laurent-t-06-1500.jpg" alt="ONE HUMAN FACTOR LIVE" class="wp-image-19" width="1500" height="1000"/></a><figcaption class="wp-element-caption">ONE HUMAN FACTOR LIVE</figcaption></figure>
-<!-- /wp:image -->
+<!-- wp:paragraph -->
+<p>Add a picture "large" and delete this paragraph</p>
+<!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left">ONE HUMAN FACTOR is mainly a live project with a lot of improvisions. The tracks are based on noise on beats.</p>
+<!-- wp:paragraph -->
+<p>ONE HUMAN FACTOR is mainly a live project with a lot of improvisions. The tracks are based on noise on beats.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right"><a href="https://01010101.one" target="_blank" rel="noreferrer noopener">https://01010101.one</a></p>
 <!-- /wp:paragraph -->
 '
         )
